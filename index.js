60f1861d0368fa44c8e467b8679e0c3f2e2d56e4
Georgy Marchuk <gmarcuk@gmail.com>
2019-04-28 23:32:33 +0200

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const baseConfig = {
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['es2015', 'stage-0']
+                }
+            }
+        ]
+    },
+    optimization: {
+        minimizer: [
+            // we specify a custom UglifyJsPlugin here to get source maps in production
+            new UglifyJsPlugin({
+                uglifyOptions: {
+                    compress: false,
+                    ecma: 6,
+                    mangle: true
+                },
+                include: /\.min\.js$/
+            })
+        ]
+    }
+};
+
+export default baseConfig;
